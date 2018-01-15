d0f9d7e60342fc6422f4c6758eb60906baf38ddd
Yahya Fadhlulloh Al-Fatih <yahya.kimochi@gmail.com>
2018-01-16 03:34:20 +0700

    batch time is complete

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ from time import sleep
 import moviepy
 
 load_dotenv(find_dotenv(), override=True)
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
 
 def getDay():
 	now = datetime.datetime.now()
@@ -39,8 +41,7 @@ def upload(filename="", content=""):
 	else:
 	    print("Can't login!")
 
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
+
 
 SENIN = os.getcwd() + "/batch/senin/"
 SELASA = os.getcwd() + "/batch/selasa/"
@@ -61,32 +62,35 @@ if platform.system() == "Windows":
 
 HARIINI = "KOSONG"
 
-if getDay() == "senin":
-	HARIINI = SENIN
-elif getDay() == "selasa":
-	HARIINI = SELASA
-elif getDay() == "rabu":
-	HARIINI = RABU
-elif getDay() == "kamis":
-	HARIINI = KAMIS
-elif getDay() == "jumat":
-	HARIINI = JUMAT
-elif getDay() == "sabtu":
-	HARIINI = SABTU
-elif getDay() == "minggu":
-	HARIINI = MINGGU
-
-path, dirs, files = os.walk(HARIINI).next()
-file_count = len(files)
-
+akhir = False
 while True:
-	if file_count > 0:
-		print "[*] Gathering job for today"
+	if akhir == False:
+		print "[*] Gathering job for today (" + getDay() + ")"
+		
+	if getDay() == "senin":
+		HARIINI = SENIN
+	elif getDay() == "selasa":
+		HARIINI = SELASA
+	elif getDay() == "rabu":
+		HARIINI = RABU
+	elif getDay() == "kamis":
+		HARIINI = KAMIS
+	elif getDay() == "jumat":
+		HARIINI = JUMAT
+	elif getDay() == "sabtu":
+		HARIINI = SABTU
+	elif getDay() == "minggu":
+		HARIINI = MINGGU
+
+	path, dirs, files = os.walk(HARIINI).next()
+	file_count = len(files)
+
+	if file_count > 0 and akhir == False:
 		a=1
 		ws = []
 		wss = []
 		lf = []	
-		txt = []
+		txt = []		
 		for f in range(0, file_count/2):
 			try:
 				formatfile = HARIINI + str(a) + ".lol"
@@ -103,29 +107,35 @@ while True:
 					txt.append(xx.rstrip())
 					uy=1
 
-			for uye in lf:
-				waktuA = datetime.datetime.strptime(str(uye), "%H:%M")
-				waktuB = datetime.datetime.strptime(datetime.datetime.now().strftime("%H:%M"), "%H:%M")
-				waktu = waktuA - waktuB
-				waktu = waktu.seconds
-				waktuS = waktuB + datetime.timedelta(seconds=waktu)
-				waktuS = waktuS.strftime("%H:%M")
-				wss.append(waktuS)
-				ws.append(int(waktu))
-
 			a=a+1
+
+		for uye in lf:
+			waktuA = datetime.datetime.strptime(str(uye), "%H:%M")
+			waktuB = datetime.datetime.strptime(datetime.datetime.now().strftime("%H:%M"), "%H:%M")
+			waktu = waktuA - waktuB
+			waktu = waktu.seconds
+			waktuS = waktuB + datetime.timedelta(seconds=waktu)
+			waktuS = waktuS.strftime("%H:%M")
+			wss.append(waktuS)
+			ws.append(int(waktu))
 
 		wss = sorted(wss)
 		ws.sort(key=int)
 		#print lf
-		print wss
+		#print wss
 
 		for xy in wss :
 			waktuA = datetime.datetime.strptime(str(xy), "%H:%M")
 			waktuB = datetime.datetime.strptime(datetime.datetime.now().strftime("%H:%M"), "%H:%M")
+			waktuC = datetime.datetime.strptime(str("00:00"), "%H:%M")
 			waktu = waktuA - waktuB
 			waktu = waktu.seconds
-			if waktu >= 86400 :
+			waktuAwal = waktuB - waktuC
+			waktuAkhir = waktuB - waktuA
+			# print waktuAwal.seconds
+			# print waktuAkhir.seconds
+			# print waktu
+			if int(waktuAkhir.seconds) <= int(waktuAwal.seconds) :
 				print "[*] Batch " + xy + " can't be prosessed because its already past time"
 			else:
 				print "[*] Next batch is " + xy + " will be posted file " 
@@ -134,11 +144,15 @@ while True:
 				sleep(10)
 				#disini posting ke instagram
 
+		akhir = True
 
 		# print ws
 		# print wss
 		# print "[*] Waiting for the next job at " + lf[0]
 		# sleep(int(waktu))
 	else:
-		print "[*] No job today, idle mode"
-		sleep(60)
+		print "[*] No job today, idle mode for " + str(os.environ.get("IDLE")) + " seconds"
+		sleep(int(os.environ.get("IDLE"))-10)
+		print "[*] 10 seconds more will do re-check the job"
+		sleep(10)
+		akhir = False
